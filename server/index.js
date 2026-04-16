c7b7676e0e8cffcdd48ce61900c99c7839541d52
Om Pradeep Bhamare <166835888+Om-1908@users.noreply.github.com>
2026-04-17 00:07:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer(app);
 
 const allowedOrigins = [
-  'http://localhost:5173',
+  'https://si-nonnas.vercel.app/',
   'http://localhost:3000',
   process.env.CLIENT_URL,  // e.g. https://sinnonnas.vercel.app
 ].filter(Boolean);
